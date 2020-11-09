97056c5410ee49892eee2849b6d356271ff75676
Emi <pieront.emilie@gmail.com>
2020-11-09 09:54:03 +0100

    modif

--- 09-misc/04-worst-ui-two/script.js
+++ 09-misc/04-worst-ui-two/script.js
@@ -12,7 +12,16 @@
 (function() {
     let result1 = parseInt(document.getElementById("part-one").innerHTML), result2 = parseInt(document.getElementById("part-two").innerHTML), result3 = parseInt(document.getElementById("part-three").innerHTML), result4 = parseInt(document.getElementById("part-four").innerHTML);
     let output = document.getElementById("target");
-    output.innerHTML = "+" + result1 + result2 + result3 + result4;
+
+    if (result2 < 10){
+        result2 = '0' + result2;
+    };
+    if (result3 < 10){
+        result3 = '0' + result3;
+    };
+    if (result4 < 10){
+        result4 = '0' + result4;
+    };
     document.getElementById("part-one").addEventListener("click", function(){
         if (result1 < this.getAttribute("data-max")){
             document.getElementById("part-one").innerHTML = ++result1;
--- 09-misc/06-guess-number/script.js
+++ 09-misc/06-guess-number/script.js
@@ -10,7 +10,37 @@
 // You will have time to focus on it later.
 
 (function() {
+    // programme ou l'utilisateur doit deviner le bon chiffre en 1 et 100
 
+function guessNumber() {
+
+    // Générer un random entre 1 et 100
+    const random = Math.floor(Math.random() * 100) + 1;
+
+    // appellle un input pour l'utilisateur
+    let number = parseInt(prompt("Guess a number from 1 to 100"));
+    let counter = 1;
+    // boucle pour définir sir le chiffre proposer et suppérieur ou inférieur avec à chaque fois une alerte plus haut ou plus bas
+    while(number != random) {
+        if (number < random) {
+            number = parseInt(prompt("heigher"));
+            ++counter;
+        }
+        else if (number > random) {
+            number = parseInt(prompt("lower"));
+        ++counter;
+        }
+    }
+    if (number == random){
+        number = prompt("thats it !!! il vous a fallu " + counter +" coups");
+    }
+    // Affiche une alerte si la valeur est correct
+    
+
+  }
+
+// appelle la fonction
+guessNumber();
     // your code here
 
 })();
